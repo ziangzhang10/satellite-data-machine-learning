f658c1a4eac674af563d02a77c933d6a7c14f7b8
ziangzhang10 <ziang.rice@gmail.com>
2019-05-11 15:41:47 -0500

    added interactiveness for geojson

--- engine/static/js/map.js
+++ engine/static/js/map.js
@@ -104,21 +104,47 @@ url2 = `/houston_geojson`;
 // for the faultlines
 //fetch("tectonicplates-master/GeoJSON/PB2002_boundaries.json")
 //  .then(function(data){
-d3.json(url2, function(data){
-    console.log(typeof(data));
-    L.geoJson(data, {
-      // Style each feature (in this case a region)
-      style: function(feature) {
-        return {
-          color: "white",
+d3.json(url2, function (data) {
+  console.log(typeof (data));
+  L.geoJson(data, {
+    // Style each feature (in this case a region)
+    style: function (feature) {
+      return {
+        color: "white",
 
-          fillOpacity: 0.5,
-          weight: 1.5
-      }
+        fillOpacity: 0.5,
+        weight: 1.5
+      };
+    },
+    // Called on each feature
+    onEachFeature: function (feature, layer) {
+      // Set mouse events to change map styling
+      layer.on({
+        // When a user's mouse touches a map feature, the mouseover event calls this function, that feature's opacity changes to 90% so that it stands out
+        mouseover: function (event) {
+          hood = event.target;
+          hood.setStyle({
+            fillOpacity: 0.9
+          });
+        },
+        // When the cursor no longer hovers over a map feature - when the mouseout event occurs - the feature's opacity reverts back to 50%
+        mouseout: function (event) {
+          hood = event.target;
+          hood.setStyle({
+            fillOpacity: 0.5
+          });
+        },
+        // When a feature (neighborhood) is clicked, it is enlarged to fit the screen
+        click: function (event) {
+          myMap.fitBounds(event.target.getBounds());
+        }
+      });
+      // Giving each feature a pop-up with information pertinent to it
+      layer.bindPopup("<h1>" + feature.properties.ID + "</h1> <hr> <h2>" + feature.properties.Legend + "</h2>");
+
     }
-    }).addTo(gerrymanders);
-  });
-
+  }).addTo(gerrymanders);
+});
 
 
 ////////////////////////////////////////////////////////////////////////////////////////
